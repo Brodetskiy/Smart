8b3c2f1e1f89e3a49a71cae3eadecfad1ba2a2e6
Igor Brodetskiy <brodsana@ukr.net>
2021-12-28 10:00:26 +0200

    add table

---
+++ DWHDarnitsa/DWHDarnitsa/Tables/PricesHistory/ANRPrices.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [PricesHistory].[ANRPrices]
+(
+	[КодМориона] int,
+	[ЦенаЗак] float,
+	[ЦенаБНДС] float,
+	[СтавкаНДС] int,
+	[Производитель] nvarchar(max),
+	[Наименование] nvarchar(max),
+	[ВнешнийКод] nvarchar(max),
+	[Партнер] nvarchar(max),
+	[ПартнерОКПО] int
+)
